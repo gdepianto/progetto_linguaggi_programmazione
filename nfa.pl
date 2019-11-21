d74b29b1ca3d0299f4e2d6e16aba3b157cf59af9
drchapman-17 <58046445+drchapman-17@users.noreply.github.com>
2019-11-21 19:40:26 +0100

    Update nfa.pl

--- nfa.pl
+++ nfa.pl
@@ -31,17 +31,17 @@ nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [seq,X,Y], gensym(FA_Id,NewId1)
                             nfa_final(NewId1, Fin1), nfa_initial(NewId2, In2),
                             assert(nfa_delta(FA_Id,Fin1, epsilon, In2)),
                             retract(nfa_final(NewId1, Fin1)), retract(nfa_initial(NewId2, In2)),
-                            rinomina_initial(NewId1, FA_Id), rinomina_deltas(NewId1, FA_Id), rinomina_deltas(NewId2, FA_Id), rinomina_final(NewId2, FA_Id).
+                            rename_initial(NewId1, FA_Id), rename_deltas(NewId1, FA_Id), rename_deltas(NewId2, FA_Id), rename_final(NewId2, FA_Id).
 
 %caso passo seq
 nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [seq,X|Xs], SubRE=.. [seq|Xs], nfa_regexp_comp(FA_Id,SubRE), %creo il sotto automa formato da seq(Xs) dove xs sono tutti gli argomenti tranne il primo
 			    gensym(FA_Id,NewId), nfa_regexp_comp(NewId,X), %creo automa per il primo
 			    nfa_final(NewId,OldFin1), nfa_initial(FA_Id,OldIn2),
-			    assert(nfa_delta(FA_Id,OldFin1,epsilon,OldIn2)), %collego il final del prim oautoma al restante
+			    assert(nfa_delta(FA_Id,OldFin1,epsilon,OldIn2)), %collego il final del primo automa al restante
 			    retract(nfa_final(NewId,OldFin1)), retract(nfa_initial(FA_Id,OldIn2)),
-			    rinomina_initial(NewId,FA_Id), rinomina_deltas(NewId,FA_Id). %rinonimo il primo automa con id correto
+			    rename_initial(NewId,FA_Id), rename_deltas(NewId,FA_Id). %rinomino il primo automa con id correto
 
-%Caso base or 
+%Caso base or
 nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [or,X,Y], gensym(FA_Id,NewId1), gensym(FA_Id,NewId2),
 			    gensym(q,FinalState),gensym(q,InitialState),
                             nfa_regexp_comp(NewId1,X), nfa_regexp_comp(NewId2,Y),
@@ -51,14 +51,14 @@ nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [or,X,Y], gensym(FA_Id,NewId1),
 			    assert(nfa_delta(FA_Id,Fin1,epsilon,FinalState)), assert(nfa_delta(FA_Id,Fin2,epsilon,FinalState)),
 			    retract(nfa_initial(NewId1,In1)), retract(nfa_initial(NewId2,In2)), retract(nfa_final(NewId1,Fin1)), retract(nfa_final(NewId2,Fin2)),
 			    assert(nfa_initial(FA_Id,InitialState)), assert(nfa_final(FA_Id, FinalState)),
-                            rinomina_deltas(NewId1, FA_Id), rinomina_deltas(NewId2, FA_Id).
+                            rename_deltas(NewId1, FA_Id), rename_deltas(NewId2, FA_Id).
 
 
-%rinomina
-rinomina_final(OldId, NewId):- nfa_final(OldId, Y), retract(nfa_final(OldId, Y)), assert(nfa_final(NewId, Y)).
-rinomina_initial(OldId, NewId):- nfa_initial(OldId, X), retract(nfa_initial(OldId, X)), assert(nfa_initial(NewId, X)).
-rinomina_delta(OldId, NewId):- nfa_delta(OldId, Q1, W, Q2), retract(nfa_delta(OldId, Q1, W, Q2)), assert(nfa_delta(NewId, Q1, W, Q2))	.
-rinomina_deltas(OldId, NewId):- forall(rinomina_delta(OldId,NewId),true).
+%rename
+rename_final(OldId, NewId):- nfa_final(OldId, Y), retract(nfa_final(OldId, Y)), assert(nfa_final(NewId, Y)).
+rename_initial(OldId, NewId):- nfa_initial(OldId, X), retract(nfa_initial(OldId, X)), assert(nfa_initial(NewId, X)).
+rename_delta(OldId, NewId):- nfa_delta(OldId, Q1, W, Q2), retract(nfa_delta(OldId, Q1, W, Q2)), assert(nfa_delta(NewId, Q1, W, Q2))	.
+rename_deltas(OldId, NewId):- forall(rename_delta(OldId,NewId),true).
 %NFA_TEST
 nfa_test(FA_Id, Input):- nfa_initial(FA_Id, S), accept(FA_Id, Input, S).
 %accept
