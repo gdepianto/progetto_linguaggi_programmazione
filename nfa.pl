354fbf41bf124bc42835dc617ead6d496a962527
Gioele De Pianto <depianto.gioele@gmail.com>
2019-11-21 19:01:31 +0100

    aggiunto seq per più di due elementi

--- nfa.pl
+++ nfa.pl
@@ -26,12 +26,24 @@ nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [star,X], nfa_regexp_comp(FA_Id
                             assert(nfa_initial(FA_Id,In)), assert(nfa_final(FA_Id,Fin)). %rende i nuovi nodi inizili e finali tali7
 %caso seq
 %caso base
-nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [seq,X,Y],!, gensym(FA_Id,NewId1), gensym(FA_Id,NewId2),
+nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [seq,X,Y], gensym(FA_Id,NewId1), gensym(FA_Id,NewId2),
                             nfa_regexp_comp(NewId1,X), nfa_regexp_comp(NewId2,Y),
                             nfa_final(NewId1, Fin1), nfa_initial(NewId2, In2),
                             assert(nfa_delta(FA_Id,Fin1, epsilon, In2)),
                             retract(nfa_final(NewId1, Fin1)), retract(nfa_initial(NewId2, In2)),
                             rinomina_initial(NewId1, FA_Id), rinomina_deltas(NewId1, FA_Id), rinomina_deltas(NewId2, FA_Id), rinomina_final(NewId2, FA_Id).
+
+
+nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [seq,X|Xs], SubRE=.. [seq|Xs], nfa_regexp_comp(FA_Id,SubRE), %creo il sotto automa formato da seq(Xs) dove xs sono tutti gli argomenti tranne il primo
+			    gensym(FA_Id,NewId), nfa_regexp_comp(NewId,X), %creo automa per il primo
+			    nfa_final(NewId,OldFin1), nfa_initial(FA_Id,OldIn2),
+			    assert(nfa_delta(FA_Id,OldFin1,epsilon,OldIn2)), %collego il final del prim oautoma al restante
+			    retract(nfa_final(NewId,OldFin1)), retract(nfa_initial(FA_Id,OldIn2)),
+			    rinomina_initial(NewId,FA_Id), rinomina_deltas(NewId,FA_Id). %rinonimo il primo automa con id correto
+
+
+
+
 %rinomina: WARNING CONTROLLARE
 rinomina_final(OldId, NewId):- nfa_final(OldId, Y), retract(nfa_final(OldId, Y)), assert(nfa_final(NewId, Y)).
 rinomina_initial(OldId, NewId):- nfa_initial(OldId, X), retract(nfa_initial(OldId, X)), assert(nfa_initial(NewId, X)).
