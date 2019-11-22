db2ec483c3132311dd3da3ec62c4ed00200ebbe6
Gioele De Pianto <depianto.gioele@gmail.com>
2019-11-22 14:30:21 +0100

    aggiunto plus

--- nfa.pl
+++ nfa.pl
@@ -56,12 +56,21 @@ nfa_regexp_comp(FA_Id, RE):- is_regexp(RE), RE=.. [or,X,Y], gensym(FA_Id,NewId1)
 nfa_regexp_comp(FA_Id, RE):- is_regexp(RE), RE=.. [or,X|Xs], SubRE=.. [or|Xs], nfa_regexp_comp(FA_Id,SubRE),
                              gensym(FA_Id,NewId), nfa_regexp_comp(NewId,X),
                              nfa_final(NewId,OldFin), nfa_initial(NewId, OldIn),
-                             nfa_final(FA_Id, NewFin), nfa_initial(FA_Id, NewIn), %qui c'era Fa_Id al posto di FA_Id
+                             nfa_final(FA_Id, NewFin), nfa_initial(FA_Id, NewIn), %qui cera Fa_Id al posto di FA_Id
                              assert(nfa_delta(FA_Id,NewIn,epsilon,OldIn)),
                              assert(nfa_delta(FA_Id,OldFin,epsilon,NewFin)),
                              retract(nfa_final(NewId,OldFin)),%cancella initial e final del nuovo ID
                              retract(nfa_initial(NewId, OldIn)),
                              rename_deltas(NewId,FA_Id).
+%CASO PLUS
+nfa_regexp_comp(FA_Id,RE):- is_regexp(RE), RE=.. [plus,X], nfa_regexp_comp(FA_Id,X), %controlla che sia star e costruisce l'automa partendo dai suoi argomenti
+                            gensym(q,In), gensym(q,Fin),  %genera un nuovo nodo iniziale e un nuovo nodo finale
+                            nfa_initial(FA_Id,OldIn), nfa_final(FA_Id,OldFin), %prende i nodi iniziali e finale dell'automa precendetemente costruito
+                            assert(nfa_delta(FA_Id,In,epsilon,OldIn)),assert(nfa_delta(FA_Id,OldFin,epsilon,Fin)),
+                            assert(nfa_delta(FA_Id,OldFin,epsilon,OldIn)), %aggiunge alla base di conoscenza i delta con epsilon mosse
+                            retract(nfa_initial(FA_Id,OldIn)), retract(nfa_final(FA_Id,OldFin)), %rende i vecchi nodi inizialie  finali dei nodi semplici
+                            assert(nfa_initial(FA_Id,In)), assert(nfa_final(FA_Id,Fin)). %rende i nuovi nodi inizili e finali tali7
+
 %rename
 rename_final(OldId, NewId):- nfa_final(OldId, Y), retract(nfa_final(OldId, Y)), assert(nfa_final(NewId, Y)).
 rename_initial(OldId, NewId):- nfa_initial(OldId, X), retract(nfa_initial(OldId, X)), assert(nfa_initial(NewId, X)).
